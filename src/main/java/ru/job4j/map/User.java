0dd69a3f7d0893d616bf669936c1112e621f3b4a
sdmserg <sdmserg2021@gmail.com>
2024-12-23 02:44:17 +0300

    4. Без переопределения equals и hashCode [#1005 #526278]. Добавление метода hashCode в класс User.

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -3,6 +3,7 @@ package ru.job4j.map;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
 
@@ -42,12 +43,18 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
     public String toString() {
         return "User{"
                 + "name = '" + name + '\''
                 + ", children = " + children
                 + ", birthday = " + birthday
                 + "}";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 
     public static void main(String[] args) {
