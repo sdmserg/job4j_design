6f06a39ff0e47f94ac162aed5c6e5347a551dfec
sdmserg <sdmserg2021@gmail.com>
2025-01-06 01:39:28 +0300

    10. Сканирование файловой системы [#106929]. Добавление классов Search, SearchFiles.

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitor;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles implements FileVisitor<Path> {
+
+    private final Predicate<Path> condition;
+
+    List<Path> result = new ArrayList<>();
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    public List<Path> getPaths() {
+        return result;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path directory,
+                                             BasicFileAttributes attributes) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path path, BasicFileAttributes attributes) throws IOException {
+        if (condition.test(path)) {
+            result.add(path);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path path, IOException exception) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path path, IOException exception) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+}
