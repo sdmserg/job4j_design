de0f73a6cb09ad23069d128f2f648d54150600da
sdmserg <sdmserg2021@gmail.com>
2025-01-06 19:13:57 +0300

    12. Валидация параметров запуска [#246865]. Изменение класса Search, добавление метода validateParams.

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,13 +10,38 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".java")).forEach(System.out::println);
+        validateParams(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validateParams(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException(
+                    "Start folder and/or file extension are not specified."
+            );
+        }
+        if (!Files.exists(Path.of(args[0]))) {
+            throw new IllegalArgumentException(String.format(
+                    "Start folder %s does not exist.", args[0])
+            );
+        }
+        if (!Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException(String.format(
+                    "%s is not directory", args[0])
+            );
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException(String.format(
+                    "%s is not file extension. "
+                    + "Enter file extension such as: .txt, .java, ...", args[1])
+            );
+        }
     }
 }
