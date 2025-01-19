ce74e9d2af5710f5b3fa27e82bd1f678852a8dbd
sdmserg <sdmserg2021@gmail.com>
2025-01-20 01:42:45 +0300

    4. Slf4j - вывод exception. [#268853]. Изменение класса UsageLog4j.

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,28 +3,37 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EchoServer {
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(9000);
-        while (!server.isClosed()) {
-            Socket socket = server.accept();
-            try (OutputStream output = socket.getOutputStream();
-                 BufferedReader input = new BufferedReader(
-                         new InputStreamReader(socket.getInputStream()))) {
-                output.write("HTTP/1.1 200 OK \r\n\r\n".getBytes());
-                String firstString = input.readLine();
-                if ("/?msg=Bye".equals(firstString.split(" ")[1])) {
-                    server.close();
-                    continue;
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream output = socket.getOutputStream();
+                     BufferedReader input = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String queryParams = input.readLine().split(" ")[1];
+                    String serverAnswer = "What";
+                    if ("/?msg=Hello".equals(queryParams)) {
+                        serverAnswer = "Hello";
+                    } else if ("/?msg=Exit".equals(queryParams)) {
+                        server.close();
+                        continue;
+                    }
+                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    output.write(serverAnswer.getBytes());
+                    output.flush();
+                } catch (IOException e) {
+                    LOG.error("Input/Output error on socket connection", e);
                 }
-                System.out.println(firstString);
-                for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                    System.out.println(string);
-                }
-                output.flush();
             }
+        }  catch (IOException e) {
+            LOG.error("Error on socket server", e);
         }
     }
 }
