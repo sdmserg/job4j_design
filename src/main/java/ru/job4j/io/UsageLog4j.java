da1b99d534b83b62e67a75f421e36c0903280d68
sdmserg <sdmserg2021@gmail.com>
2025-01-21 01:01:39 +0300

    3. Slf4j - вывод переменных. [#268852 #529178]. Изменение класса UsageLog4j.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -9,7 +9,7 @@ public class UsageLog4j {
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
-        Long number = 161524312434L;
+        long number = 161524312434L;
         int age = 33;
         double salary = 120000.50;
         float experience = 2.5f;
@@ -17,12 +17,11 @@ public class UsageLog4j {
         char firstLetter = 'P';
         short officeNumber = 650;
         byte skillLevel = 100;
-
         LOG.debug("User info name: {}, age: {}", name, age);
         LOG.info("User name: {}, number: {}, age: {}, salary: {}, "
-                + "experience: {}, isDeveloper: {}, "
-                + "first character of name: {}, office number: {}, "
-                + "level of skill: {}",
+                        + "experience: {}, isDeveloper: {}, "
+                        + "first character of name: {}, office number: {}, "
+                        + "level of skill: {}",
                 name, number, age, salary,
                 experience, isDeveloper,
                 firstLetter, officeNumber,
